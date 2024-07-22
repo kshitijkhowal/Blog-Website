75bdd8dc865e4628e2b64941760f90ccc97a1e7e
kshitijkhowal <kshitij.durgesh@gmail.com>
2024-07-22 12:49:03 +0530

    newpost page

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const app=express();
 const port=3000;
 
 
+
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
+
+app.get("/addPost",(req,res)=>{
+    res.render("newPost.ejs");
+})
 
 app.use(express.static('public'));
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
+
